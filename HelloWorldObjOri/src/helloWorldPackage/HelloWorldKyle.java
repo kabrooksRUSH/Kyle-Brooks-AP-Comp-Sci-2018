5ce66d37a2c18781af1cc8865a49a6d8b8d111af
kabrooksRUSH <kylebrooks0125@gmail.com>
2017-08-30 16:52:52 -0400

    Object Oriented Hello World

---
+++ HelloWorldObjOri/src/helloWorldPackage/HelloWorldKyle.java
@@ -0,0 +1,13 @@
+package helloWorldPackage;
+
+public class HelloWorldKyle {	
+	private String message;
+	
+	public HelloWorldKyle(String m) {
+		message = m;
+	}
+	
+	public String getMessage(){
+		return message;
+	}
+}
---
+++ HelloWorldObjOri/src/helloWorldPackage/Main.java
@@ -0,0 +1,9 @@
+package helloWorldPackage;
+
+
+public class Main {
+	public static void main(String[] args){
+		HelloWorldKyle Hello = new HelloWorldKyle("Hello World Kyle");
+		System.out.print(Hello.getMessage());
+	}
+}
