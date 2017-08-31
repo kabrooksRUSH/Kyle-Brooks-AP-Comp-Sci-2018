250c5b8c519c5f506a767f9eb9396458b36ae3af
kabrooksRUSH <kylebrooks0125@gmail.com>
2017-08-31 19:30:49 -0400

    Added method greet()

--- HelloWorldObjOri/src/helloWorldPackage/HelloWorldKyle.java
+++ HelloWorldObjOri/src/helloWorldPackage/HelloWorldKyle.java
@@ -3,11 +3,23 @@ package helloWorldPackage;
 public class HelloWorldKyle {	
 	private String message;
 	
+	static long epoch = System.currentTimeMillis()/1000;
+	static String date = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date (epoch*1000));
+	
 	public HelloWorldKyle(String m) {
 		message = m;
 	}
 	
 	public String getMessage(){
 		return message;
+	}
+	
+	public String getTime(){
+		return date;
+	}
+	
+	public String greet(){
+		String greet;
+		return greet = message + " the time is " + date;
 	}
 }
--- HelloWorldObjOri/src/helloWorldPackage/Main.java
+++ HelloWorldObjOri/src/helloWorldPackage/Main.java
@@ -1,9 +1,10 @@
 package helloWorldPackage;
 
-
 public class Main {
 	public static void main(String[] args){
-		HelloWorldKyle Hello = new HelloWorldKyle("Hello World Kyle");
-		System.out.print(Hello.getMessage());
+		//HelloWorldKyle Hello = new HelloWorldKyle("Hello World Kyle");
+		//System.out.print(Hello.greet());	
+		HelloWorldKyle Marlena = new HelloWorldKyle("Marlena");
+		System.out.print(Marlena.greet());
 	}
 }
